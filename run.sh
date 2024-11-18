68f0a3e2e06baf5db266c7742db7271948dcee1f
smakuch0 <szymon.d.makuch@gmail.com>
2024-11-18 22:21:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,45 +1,14 @@
 #!/bin/bash
 
 echo ==============================
-echo C server - Python client combo
+echo C server - C client combo
 echo ==============================
 
 (
   cd ./C/server
   docker build -t z34_c_server .
-  docker run --network z34_network --name z34_c_server --network-alias z34_c_server --hostname z34_c_server z34_c_server:latest 8000
+  docker run --network z34_network --name z34_c_server --network-alias z34_c_server z34_c_server:latest 8000
 ) &
-
-sleep 2
-
-(
-    cd ./Python/client
-    docker build -t z34_p_client .
-    docker run --network z34_network --name z34_p_client z34_p_client z34_p_server 8000
-) &
-
-sleep 7
-
-docker logs -f z34_c_server &
-docker logs -f z34_p_client &
-
-sleep 3
-
-docker rm -f z34_c_server z34_p_client 2>/dev/null || true
-
-sleep 3
-
-echo ==============================
-echo Python server - C client combo
-echo ==============================
-
-(
-    cd ./Python/server
-    docker build -t z34_p_server .
-    docker run --network z34_network --name z34_p_server --hostname z34_p_server z34_p_server   
-) &
-
-sleep 2
 
 (
   cd ./C/client
@@ -49,9 +18,36 @@ sleep 2
 
 sleep 7
 
-docker logs -f z34_p_server &
+docker logs -f z34_c_server &
 docker logs -f z34_c_client &
 
 sleep 3
 
-docker rm -f z34_p_server z34_c_client 2>/dev/null || true
+docker rm -f z34_c_server z34_c_client 2>/dev/null || true
+
+sleep 3
+
+echo ==============================
+echo C server - C client combo
+echo ==============================
+
+(
+    cd ./Python/server
+    docker build -t z34_p_server .
+    docker run --network z34_network --name z34_p_server --hostname z34_p_server z34_p_server   
+) &
+
+(
+    cd ./Python/client
+    docker build -t z34_p_client .
+    docker run --network z34_network --name z34_p_client z34_p_client z34_p_server 8000
+) &
+
+sleep 7
+
+docker logs -f z34_p_server &
+docker logs -f z34_p_client &
+
+sleep 3
+
+docker rm -f z34_p_server z34_p_client 2>/dev/null || true
