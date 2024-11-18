05909fd2126cabd4d8011166fadb69706c656cc1
smakuch0 <szymon.d.makuch@gmail.com>
2024-11-18 22:12:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,40 +1,13 @@
 #!/bin/bash
 
-echo ============
-echo C-C combo
-echo
+echo ==============================
+echo C server - Python client combo
+echo ==============================
 
 (
   cd ./C/server
   docker build -t z34_c_server .
   docker run --network z34_network --name z34_c_server --network-alias z34_c_server z34_c_server:latest 8000
-) &
-
-(
-  cd ./C/client
-  docker build -t z34_c_client .
-  docker run --network z34_network --name z34_c_client z34_c_client:latest z34_c_server 8000
-) &
-
-sleep 7
-
-docker logs -f z34_c_server &
-docker logs -f z34_c_client &
-
-sleep 3
-
-docker rm -f z34_c_server z34_c_client 2>/dev/null || true
-
-sleep 3
-
-echo ===========
-echo Python - Python combo
-echo
-
-(
-    cd ./Python/server
-    docker build -t z34_p_server .
-    docker run --network z34_network --name z34_p_server --hostname z34_p_server z34_p_server   
 ) &
 
 (
@@ -45,9 +18,36 @@ echo
 
 sleep 7
 
-docker logs -f z34_p_server &
+docker logs -f z34_c_server &
 docker logs -f z34_p_client &
 
 sleep 3
 
-docker rm -f z34_p_server z34_p_client 2>/dev/null || true
+docker rm -f z34_c_server z34_p_client 2>/dev/null || true
+
+sleep 3
+
+echo ==============================
+echo Python server - C client combo
+echo ==============================
+
+(
+    cd ./Python/server
+    docker build -t z34_p_server .
+    docker run --network z34_network --name z34_p_server --hostname z34_p_server z34_p_server   
+) &
+
+(
+  cd ./C/client
+  docker build -t z34_c_client .
+  docker run --network z34_network --name z34_c_client z34_c_client:latest z34_c_server 8000
+) &
+
+sleep 7
+
+docker logs -f z34_p_server &
+docker logs -f z34_c_client &
+
+sleep 3
+
+docker rm -f z34_p_server z34_c_client 2>/dev/null || true
