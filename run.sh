79659d0c49e886202be2561509a38790032d40e8
smakuch0 <szymon.d.makuch@gmail.com>
2024-11-18 21:59:18 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,3 +18,24 @@ docker logs -f z34_c_server &
 docker logs -f z34_c_client &
 
 docker rm -f z34_c_server z34_c_client z34_p_server z34_p_client 2>/dev/null || true
+
+wait 1
+
+(
+    cd ./Python/server
+    docker build -t z34_p_server .
+    docker run --network z34_network --name z34_p_server --hostname z34_p_server z34_p_server   
+) &
+
+(
+    cd ./Python/client
+    docker build -t z34_p_client .
+    docker run --network z34_network --name z34_p_client z34_p_client z34_p_server 8000
+) &
+
+sleep 5
+
+docker logs -f z34_c_server &
+docker logs -f z34_c_client &
+
+docker rm -f z34_c_server z34_c_client z34_p_server z34_p_client 2>/dev/null || true
