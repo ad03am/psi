fc4c6c8c715565141fac3b4110be131f261423ff
smakuch0 <szymon.d.makuch@gmail.com>
2024-11-18 17:50:24 +0100

    C

---
+++ compose_UDP/sink/udp_sink2.c
@@ -0,0 +1,84 @@
+/* (c) Grzegorz Blinowski 2000-2023 [ PSI] */
+/* this example from: getaddrinfo man page */
+
+#include <err.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#define BUF_SIZE 70000
+
+#define bailout(s) { perror( s ); exit(1);  }
+#define Usage() { errx( 0, "Usage: %s [port]\n", argv[0]); }
+#define timeinms(tv) ( (tv.tv_sec) * 1000.0 + (tv.tv_usec) / 1000.0 )
+
+int validate_datagram(const char *buf, int length) {
+    unsigned int reported_length = ((buf[0] << 8) | buf[1]) & 0xFFFF;
+    int i = 2;
+    if (reported_length != length-2) {
+        fprintf(stderr, "Length mismatch: reported %d, actual %d\n", reported_length, length);
+        return 0;
+    }
+
+    for (i; i < length; i++) {
+        if (buf[i] != 'A' + ((i-2) % 26)) {
+            fprintf(stderr, "Content mismatch at byte %d: %d vs %d\n", i, buf[i], 'A' + ((i-2) % 26));
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    int                      sfd, s;
+    char                     buf[BUF_SIZE], reply[] = "OK";
+    ssize_t                  nread;
+    struct sockaddr_in       server, client;
+    socklen_t                client_len = sizeof(client);
+
+    if (argc != 2) 
+        Usage();
+
+    if ( (sfd=socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	bailout("socker() ");
+
+   server.sin_family      = AF_INET;  /* Server is in Internet Domain */
+   server.sin_port        = htons(atoi(argv[1]));         /* Use any available port      */
+   server.sin_addr.s_addr = INADDR_ANY; /* Server's Internet Address   */
+
+   if ( (s=bind(sfd, (struct sockaddr *)&server, sizeof(server))) < 0)
+      bailout("bind() ");
+   printf("bind() successful\n");
+
+
+    /* Read datagrams and echo them back to sender. */
+   printf("waiting for alphabet...\n");
+
+    for (;;) {
+        nread = recvfrom(sfd, buf, BUF_SIZE, 0, (struct sockaddr *) &client, &client_len);
+        if (nread <0 ) {
+            fprintf(stderr, "failed recvfrom\n");
+            continue;               /* Ignore failed request */
+        }
+        printf("Received %d bytes datagram\n", nread);
+
+        if (validate_datagram(buf, nread)) {
+            sendto(sfd, reply, sizeof(reply), 0, (struct sockaddr *)&client, client_len);
+            printf("Datagram validated, response sent.\n");
+        } else {
+            fprintf(stderr, "Invalid datagram\n");
+            sendto(sfd, reply, sizeof(reply), 0, (struct sockaddr *)&client, client_len);
+            continue;
+        }
+    }
+
+    printf("Close sink...\n");
+    close(sfd);
+    exit(0);
+}
---
+++ compose_UDP/source/udp_source2.c
@@ -0,0 +1,92 @@
+#include "./udp_source_sink.h"
+#include <arpa/inet.h>
+#include <err.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define bailout(s) {perror(s); exit(1); }
+#define Usage() { errx( 0, "Usage: %s <host> <port>\n", argv[0]); }
+#define timeinms(tv) ( (tv.tv_sec) * 1000.0 + (tv.tv_usec) / 1000.0 )
+
+#define TEST_MAX_SIZE 70000
+
+void prepare_dgram(char *databuf, int length) {
+    databuf[0] = (length >> 8) & 0xFF;
+    databuf[1] = length & 0xFF;
+    int i = 2;
+
+    int reported_length = ((databuf[0] << 8) | databuf[1]) & 0xFFFF;
+    for (i; i <= length+2; i++) {
+        databuf[i] = 'A' + ((i - 2) % 26);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    struct hostent *hp;
+    char databuf[TEST_MAX_SIZE];
+    char recvbuf[1024];
+    int dgram_size = 32768;
+    socklen_t addr_len = sizeof(server);
+
+
+    if ( argc !=3) {
+        Usage();
+    }
+
+    /* Create socket. */
+    sock = socket( AF_INET, SOCK_DGRAM, 0 );
+    if (sock == -1)
+        bailout("opening stream socket");
+
+    /* uzyskajmy adres IP z nazwy . */
+    server.sin_family = AF_INET;
+    hp = gethostbyname2(argv[1], AF_INET );
+
+    /* hostbyname zwraca strukture zawierajaca adres danego hosta */
+    if (hp == (struct hostent *) 0) {
+        errx(2, "%s: unknown host\n", argv[1]);
+    }
+    printf("address resolved...\n");
+
+    memcpy((char *) &server.sin_addr, (char *) hp->h_addr, hp->h_length);
+    server.sin_port = htons(atoi( argv[2]) );
+
+    connect (sock, (struct sockaddr *) &server, sizeof(server) );
+    printf("sending the alphabet...\n");
+
+    while (dgram_size <= TEST_MAX_SIZE) {
+        ssize_t nread;
+        prepare_dgram(databuf, dgram_size);
+
+        printf("Sending %d bytes datagram...\n", dgram_size);
+        if ((send( sock, databuf, dgram_size+2, 0 )) <0 ) {
+            perror("send error!\n");
+            break;
+	    }
+
+        nread = recvfrom(sock, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *) &server, &addr_len);
+        if (nread < 0 ) {
+	        fprintf(stderr, "failed recvfrom\n");
+            continue;               /* Ignore failed request */
+	    }
+        else {
+            printf("Received %d bytes from server\n", nread);
+        }
+
+        dgram_size += 1;
+	}
+
+    printf("End this...\n");
+    close(sock);
+    exit(0);
+}
